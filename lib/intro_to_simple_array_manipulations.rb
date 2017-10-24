7143118a01d283087dbccc8b9e79c9ef36509a29
Matt Faircloth <mattcfaircloth@gmail.com>
2017-10-24 16:02:23 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -25,3 +25,19 @@ end
 def using_concat(array1, array2)
   array1.concat(array2)
 end
+
+def using_insert(array, element)
+  array.insert(4, element)
+end
+
+def using_uniq(array)
+
+end
+
+def using_flatten(array)
+
+end
+
+def using_delete(array, integer)
+
+end
